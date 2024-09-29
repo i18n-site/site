b7fa68945ea37de571650e67ccbb8c3b8d472fa7
i18n <i18n.site@gmail.com>
2024-09-29 16:25:53 +0800

    Merge branch 'dev' of atomgit.com:i18n/site into dev

--- sh/svg.sh
+++ sh/svg.sh
@@ -10,6 +10,11 @@ zip() {
   bun x svgo --config $DIR/svgo.config.cjs -r -f $1 -o $1
 }
 
+cd file/var
+find . -name "*.svg" -exec sed -i 's/class="icon"//g' {} \;
+
+cd $DIR/..
+
 zip file
 zip public
 cd $DIR
