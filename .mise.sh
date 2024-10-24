f9be2c659ff40524414da1ed3ed204aadfe3aed0
i18n <i18n.site@gmail.com>
2024-10-25 01:25:06 +0800

    🔥

--- .mise.sh
+++ .mise.sh
@@ -5,12 +5,14 @@ if echo ":$PATH:" | grep -q ":$DIR/.mise/bin:"; then
 fi
 
 cd $DIR
+
 if [ ! -d "conf" ]; then
   git clone --depth=1 $(git config --get remote.origin.url | sed 's/\.git$//').conf.git conf
 fi
 
 pj=package.json
-
 if [ ! -f "$pj" ]; then
+  bun i @3-/read @3-/write @3-/yml lodash-es @3-/zx
+  rm package.json
   ./sh/install.js
 fi
